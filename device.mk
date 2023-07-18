996743020483a58221bd61f6f670d8c7335cd0dd
Bruno Martins <bgcngm@gmail.com>
2023-07-18 10:29:25 +0100

    diting: Introduce support for Euicc
    The following commit is a squash of:
    
    diting: Ship prebuilt EuiccGoogle from Pixel 7a (lynx)
    
    diting: overlay: Set slotId# of non-removable esim
    
       This is needed for esim to function properly.
       Without it the EID value could not be read.
    
    diting: overlay: Declare eUICC device capabilities
    
    diting: overlay: Set sim slot mappings for esim
    
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: Sugakesshaa <sugakesshaa@pixelos.net>
    Change-Id: I4e1c8250c7831d7f84a7cc55c3d5e1c9e767fd10

--- device.mk
+++ device.mk
@@ -13,6 +13,10 @@ $(call inherit-product, vendor/xiaomi/diting/diting-vendor.mk)
 # MiuiCamera
 $(call inherit-product, vendor/xiaomi/camera/miuicamera.mk)
 
+# Euicc
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
 # Kernel
 TARGET_KERNEL_DIR := $(LOCAL_PATH)-kernel
 TARGET_BOARD_KERNEL_HEADERS := $(LOCAL_PATH)-kernel/kernel-headers
@@ -20,6 +24,7 @@ TARGET_BOARD_KERNEL_HEADERS := $(LOCAL_PATH)-kernel/kernel-headers
 # Overlay
 PRODUCT_PACKAGES += \
     ApertureResDiting \
+    EuiccResDiting \
     FrameworksResDiting \
     NfcResDiting \
     SettingsProviderResDiting \

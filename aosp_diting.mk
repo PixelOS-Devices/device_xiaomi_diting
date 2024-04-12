928afbc6b6b458d31324b487dbcba8d233e261e0
Sugakesshaa <sugakesshaa@pixelos.net>
2024-04-12 22:52:12 +0800

    diting: Update from HyperOS V816.0.3.0.ULFMIXM
    Change-Id: I88a27795eb19a6a79388f4146c80481629d6057e

--- aosp_diting.mk
+++ aosp_diting.mk
@@ -21,10 +21,10 @@ PRODUCT_SYSTEM_NAME := diting_global
 PRODUCT_SYSTEM_DEVICE := diting
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="ditingp_global-user 12 SKQ1.230401.001 V816.0.2.0.ULFMIXM release-keys" \
+    PRIVATE_BUILD_DESC="ditingp_global-user 12 SKQ1.230401.001 V816.0.3.0.ULFMIXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := Xiaomi/ditingp_global/diting:12/SKQ1.230401.001/V816.0.2.0.ULFMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/ditingp_global/diting:12/SKQ1.230401.001/V816.0.3.0.ULFMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
